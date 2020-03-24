4e3ff5b2edd50baa90c5d56d14dde88fcf68ed18
hungprok <57722641+hungprok@users.noreply.github.com>
2020-03-24 10:04:35 +0700

    fourth

--- src/App.js
+++ src/App.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import MovieCard from './Component/MovieCard';
-import LatestMovie from './Component/LatestMovie';
-import Slider from './Component/Slider';
-import MainPage from './Component/MainPage';
-import DropDown from './Component/DropDown';
-import ReactModal from 'react-modal';
-import YouTube from '@u-wave/react-youtube';
-=======
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -20,74 +8,54 @@ import MainPage from "./Component/MainPage";
 import DropDown from "./Component/DropDown";
 import ReactModal from "react-modal";
 import YouTube from "@u-wave/react-youtube";
->>>>>>> 35cdd9983811d15fce4c6a0e39403a1259f8b426
 
+const api = "5c4d91615c827590b1f7965784c32a2c";
 let keyword = "";
+let url=`https://api.themoviedb.org/3/discover/movie?api_key=${api}&language=en-US&sort_by=popularity.desc&page=1`
 
 function Render() {
-  let [modal, setModal] = useState(false);
-  let [movietrailer, setTrailer] = useState([]);
   let [movieInfo, setmovieInfo] = useState(null);
-<<<<<<< HEAD
-  let [key, setKey] = useState('');
-=======
   let [modal, setModal] = useState(false);
-  // let [video, setVideo] = useState([]);
   let [key, setKey] = useState("");
->>>>>>> 35cdd9983811d15fce4c6a0e39403a1259f8b426
-  let mainPageNumber = 1;
-  let sortType = "popularity";
-  const api = "5c4d91615c827590b1f7965784c32a2c";
+  let [mainPageNumber, setMainPageNumber] = useState(1);
+  let [sortType, setSortType] = useState("popularity");
+  let [sortBy, setSortBy] = useState("desc");
+  // let [url, setUrl] = useState(`https://api.themoviedb.org/3/discover/movie?api_key=${api}&language=en-US&sort_by=${sortType}.${sortBy}&page=${mainPageNumber}`);
 
-<<<<<<< HEAD
-let onhandleSetModal = idcard => {
-  turnModal(idcard)
-}
-
-let turnModal = async (id) => {
-  console.log(id)
-  let url = `https://api.themoviedb.org/3/movie/${id}/videos?api_key=${api}&language=en-US`;
-  let data = await fetch(url);
-  let response = await data.json();
-  let result = response.results;
-  if (result && result !== []) {setTrailer(result)} else {turnModal(id)}
-  trailer();
-  setModal(true);
-};
-
-  let trailer = () => {
-    let keychoice = movietrailer
-    console.log(keychoice);
-    // setKey(keychoice.key)
-  }
-
-  let changePage = (newPageNum) => {
-=======
-  let changePage = newPageNum => {
->>>>>>> 35cdd9983811d15fce4c6a0e39403a1259f8b426
-    mainPageNumber = newPageNum;
-    fetchMovieInfo(mainPageNumber);
+  let fetchMovieInfo = async () => {
+    console.log(mainPageNumber);
+    let data = await fetch(url);
+    let result = await data.json();
+    setmovieInfo(result);
   };
 
-  let changeSortType = type => {
-    sortType = type;
+  let changePage = (newPageNum) => {
+    console.log(newPageNum);
+    setMainPageNumber(newPageNum);
+    url = `https://api.themoviedb.org/3/discover/movie?api_key=${api}&language=en-US&sort_by=${sortType}.${sortBy}&page=${newPageNum}`;
+    fetchMovieInfo(url);
+    console.log(url);
+  };
+
+  let changeSortType = (type) => {
+    setSortType(type);
+    url = `https://api.themoviedb.org/3/discover/movie?api_key=${api}&language=en-US&sort_by=${type}.${sortBy}&page=${mainPageNumber}`;
     fetchMovieInfo(sortType);
   };
 
-  let sortBy = "desc";
-  let changeSortBy = type => {
-    if (type) {
-      sortBy = "asc";
-    } else {
-      sortBy = "desc";
-    }
+  let changeSortBy = (type) => {
+    setSortBy(type);
+    url = `https://api.themoviedb.org/3/discover/movie?api_key=${api}&language=en-US&sort_by=${sortType}.${type}&page=${mainPageNumber}`;
     fetchMovieInfo(sortBy);
   };
 
-  // let search = (keyword) => {
-  //   url =`https://api.themoviedb.org/3/search/movie?query=${keyword}?api_key=${api}&language=en-US`;
-  //   fetchMovieInfo(url)
-  // };
+  let search = async (keyword) => {
+    let url = `https://api.themoviedb.org/3/search/movie?query=${keyword}?api_key=${api}&language=en-US`;
+    let data = await fetch(url);
+    let result = await data.json();
+    setmovieInfo(result);
+  };
+
   let randomVideo = (trailer) => {
     let listRanVideo = Object.keys(trailer);
     let randomIndex = Math.floor(Math.random() * listRanVideo.length);
@@ -114,66 +82,9 @@ let turnModal = async (id) => {
     }
   };
 
-  let fetchMovieInfo = async () => {
-    let url = `https://api.themoviedb.org/3/discover/movie?api_key=${api}&language=en-US&sort_by=${sortType}.${sortBy}&page=${mainPageNumber}`;
-    let data = await fetch(url);
-    let result = await data.json();
-<<<<<<< HEAD
-
-    // console.log(mainPageNumber);
-    // console.log(result);
-=======
->>>>>>> 35cdd9983811d15fce4c6a0e39403a1259f8b426
-    setmovieInfo(result);
-  };
-
   useEffect(fetchMovieInfo, []);
 
   if (!movieInfo) {
-<<<<<<< HEAD
-    return <div>Loading</div>
-  };
-
-  return (<div>
-    <div style={{ backgroundColor: "#333333" }}>
-      <nav className="navbar justify-content-between " style={{ backgroundImage: "linear-gradient(#00000090,#333333)", height: 100 }}>
-        <div className="container p-0">
-          <a className="navbar-brand" href="index.html">
-            <img
-              height="60px"
-              src="https://upload.wikimedia.org/wikipedia/commons/6/6a/Cinemax_LA.png"
-              alt=" "
-            />
-          </a>
-          <form className="form-inline ">
-            <input onChange={(e) => { keyword = e.target.value; console.log(keyword) }}
-              className="form-control mr-sm-2 "
-              type="search "
-              placeholder="Type something "
-              aria-label="Search "
-            />
-            <button className="btn btn-outline-light my-2 my-sm-0 " type="submit ">
-              Search
-      </button>
-          </form>
-        </div>
-      </nav>
-    </div>
-    <div style={{ backgroundColor: "#333333" }}>
-      <div className="container ">
-        <div className="row ">
-          <div className="col-md-9 p-0 ">
-            <Slider />
-            <div className="row">
-              <div className="col-6 p-0 pl-1 d-flex">
-                <DropDown sortType={changeSortType} />
-                <button onClick={() => changeSortBy(true)} className="btn btn-outline-light m-1" type="submit ">↑</button>
-                <button onClick={() => changeSortBy(false)} className="btn btn-outline-light mt-1 mb-1" type="submit ">↓</button>
-              </div>
-              <div className="col-6">
-                <div class="row m-0 p-0 mt-1 mb-1 d-flex justify-content-end">
-                  <MainPage PassFunction={changePage} />
-=======
     return <div>Loading</div>;
   }
 
@@ -188,12 +99,11 @@ let turnModal = async (id) => {
           }}
         >
           <ReactModal
-          onRequestClose={() => setModal(false)}
+            onRequestClose={() => setModal(false)}
             isOpen={modal}
-            closeTimeoutMS={200}
             style={{
               overlay: { display: "flex", justifyContent: "center" },
-              content: { width: "70%", height: "70%", position: "relative" },
+              content: { width: "70%", height: "70%", position: "relative" }
             }}
           >
             <YouTube video={key} width='100%' height='100%' autoplay />
@@ -217,10 +127,9 @@ let turnModal = async (id) => {
                 placeholder="Type something "
                 aria-label="Search "
               />
-              <button
+              <button onClick={() => search(keyword)}
                 className="btn btn-outline-light my-2 my-sm-0 "
-                type="submit "
-              >
+                type="submit ">
                 Search
               </button>
             </form>
@@ -254,7 +163,6 @@ let turnModal = async (id) => {
                   <div class="row m-0 p-0 mt-1 mb-1 d-flex justify-content-end">
                     <MainPage PassFunction={changePage} />
                   </div>
->>>>>>> 35cdd9983811d15fce4c6a0e39403a1259f8b426
                 </div>
               </div>
               <div className="row m-0 p-0 justify-content-between ">
@@ -267,22 +175,11 @@ let turnModal = async (id) => {
                 <MainPage PassFunction={changePage} />
               </div>
             </div>
-<<<<<<< HEAD
-            <div className="row m-0 p-0 justify-content-between ">
-              <MovieCard MovieInformation={movieInfo.results} onhandleSetModal={onhandleSetModal}/>
-              <ReactModal video={key} isOpen={modal} onRequestClose={()=>setModal(false)} style={{overlay:{width: "70%", height: "70%"}, contents:{}}}>
-              <YouTube video="x2to0hs" autoplay/>
-              </ReactModal>
-            </div>
-            <div class="row m-0 p-0 mt-1 d-flex justify-content-end">
-              <MainPage PassFunction={changePage} />
-=======
             <div className="col-md-3 p-0 pl-5">
               <h4 className="text-white text-center mt-0 mb-3">
                 ALL TIME RANKING
               </h4>
               <LatestMovie />
->>>>>>> 35cdd9983811d15fce4c6a0e39403a1259f8b426
             </div>
           </div>
         </div>
--- src/Component/MovieCard.js
+++ src/Component/MovieCard.js
@@ -20,11 +20,7 @@ export default function MovieCard(props) {
                     }}>{item.title}</h6>
                     <div className="row">
                     <div className="col-5 m-0 p-0 pl-1">
-<<<<<<< HEAD
-                    <h6 className="text-left m-0 pl-1"><button onClick={()=> props.onhandleSetModal(item.id)}>PLAY</button></h6>
-=======
                     <h6 onClick={() => props.handleOnModal(item.id)} style={{cursor: 'pointer'}} className="text-left m-0 pl-1"> <img style={{ height: 26 }} src="https://optimusdivi.com/wp-content/uploads/2018/07/Play-Icon-Logo-4.svg" alt=""/> PLAY</h6>
->>>>>>> 35cdd9983811d15fce4c6a0e39403a1259f8b426
                         </div>
                         <div className="col-7 ">
                         <h6 className="text-right m-0"><img style={{ height: 22 }} src="https://www.archiveteam.org/images/e/e6/Imdb_logo.png" alt="" /> {item.vote_average}</h6></div>
